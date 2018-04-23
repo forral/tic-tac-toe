a996616845469188b4dab210a80b15704bdb6f59
forral <forra.luis@me.com>
2018-04-23 21:29:41 +0100

    the board now shows the unavaiable fields and updates an object

--- scripts.js
+++ scripts.js
@@ -1,7 +1,23 @@
 var grid = document.querySelector('.grid');
 
 grid.addEventListener('click', function(e) {
-  if (e.target.dataset.number) {
-    console.log(e.target.dataset.number);
+  if (e.target.dataset.number && e.target.dataset.status === 'free') {
+    console.log(e.target.dataset.number, e.target.dataset.status);
+    e.target.dataset.status = 'used';
+    e.target.classList += ' ' + 'used';
+    board[e.target.dataset.number] = 'X';
+    console.table(board);
   }
 });
+
+var board = {
+  1: false,
+  2: false,
+  3: false,
+  4: false,
+  5: false,
+  6: false,
+  7: false,
+  8: false,
+  9: false
+}
