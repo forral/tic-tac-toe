0b70ab255a0ad5f5b8b21ccb8e175f766b7da74c
forral <forra.luis@me.com>
2018-04-25 16:48:12 +0100

    basic functionality of the checkWinner function added

--- scripts.js
+++ scripts.js
@@ -3,11 +3,10 @@ var fields = document.getElementsByClassName('field');
 fields = [...fields];
 
 var counter = 0;
-
 var board = {
-  1: false,
-  2: false,
-  3: false,
+  1: 'X',
+  2: 'X',
+  3: 'X',
   4: false,
   5: false,
   6: false,
@@ -28,8 +27,8 @@ grid.addEventListener('click', function(e) {
 
   if (e.target.dataset.number && e.target.dataset.status === 'free') {
     console.log(e.target.dataset.number, e.target.dataset.status);
-    e.target.dataset.status = 'used';
-    e.target.classList += ' ' + 'used';
+    e.target.dataset.status = 'used-by-player-one';
+    e.target.classList += ' ' + 'used-by-player-one';
     board[e.target.dataset.number] = 'X';
     console.table(board);
     counter++;
@@ -38,18 +37,16 @@ grid.addEventListener('click', function(e) {
       counter = 0;
       resetBoard();
     } else {
-      AI();
+      AIRound();
     }
-
   }
-
-
 });
 
 function resetBoard() {
   // remove all the markes from the board.
   fields.forEach(function (field) {
-      field.classList.remove('used');
+      field.classList.remove('used-by-player-one');
+      field.classList.remove('used-by-player-two');
       field.dataset.status = 'free';
     });
 
@@ -62,7 +59,7 @@ function resetBoard() {
   console.table(board);
 }
 
-function AI() {
+function AIRound() {
   
   var played = false;
 
@@ -72,12 +69,38 @@ function AI() {
     // 2. see if the space on the grid is avaiable.
     if (board[spot] === false) {
       // if so, market the spot.
-      fields[spot - 1].dataset.status = 'used';
-      fields[spot - 1].classList += ' ' + 'used';
+      fields[spot - 1].dataset.status = 'used-by-player-two';
+      fields[spot - 1].classList += ' ' + 'used-by-player-two';
       board[spot] = 'O';
       console.table(board);
       counter++;
       played = true;
+    }
+  }
+}
+
+function checkWinner(player) {
+  // if any of the players can make one of these sequences, that player wons.
+  var winFieldPositions = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+  var playerOne = player;
+  var counterTwo;
+
+  for (var i = 0; i < winFieldPositions.length; i++) {
+
+    counterTwo = 0;
+
+    for (var j = 0; j < winFieldPositions[i].length; j++) {
+
+      if (board[winFieldPositions[i][j]] === playerOne) {
+        counterTwo++
+      }
+
+      if (counterTwo === 3) {
+        console.log('Winner');
+        return;
+      }
+
+
     }
   }
 }
